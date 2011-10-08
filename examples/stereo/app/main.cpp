9078f5bb1a64fdcb58e8b2d68374fd400464a914
andrey.morozov <andrey.morozov@itseez.com>
2011-10-08 18:04:48 +0400

    changed parameters of algorithm

--- examples/stereo/app/main.cpp
+++ examples/stereo/app/main.cpp
@@ -13,21 +13,21 @@ int main(int argc, const char ** argv)
 	Settings settings(argc, argv);
 	if (settings.is_exit()) return 0;
 
-        Mat r = cv::imread(settings.right(), 0);
-        Mat l = cv::imread(settings.left() , 0);
+    Mat r = cv::imread(settings.right(), 0);
+    Mat l = cv::imread(settings.left() , 0);
 
-        StereoBeliefPropagation csbp(128, 11, 5);
+    StereoBeliefPropagation csbp(96, 7, 5);
 
-        GpuMat disparity(l.size(), CV_8U);
-        GpuMat left  (l);
-        GpuMat right (r);
+    GpuMat disparity(l.size(), CV_8U);
+    GpuMat left  (l);
+    GpuMat right (r);
 
-        csbp(left, right, disparity);
+    csbp(left, right, disparity);
 
-        Mat disp;
-        disparity.download(disp);
+    Mat disp;
+    disparity.download(disp);
 
-        cv::imwrite(settings.result(), disp);
+    cv::imwrite(settings.result(), disp);
 
 	return 0;
 }
