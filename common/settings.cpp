22bb209a94c3ee55f9fc64714d358df6715aeabe
AoD314 <morozov.andrey.vmk@gmail.com>
2011-12-19 15:16:21 +0400

    added implement Settings class on QSetting base

--- common/settings.cpp
+++ common/settings.cpp
@@ -1,4 +1,38 @@
 
 #include "settings.hpp"
 
-Settings::Settings(int argc, char ** argv) {}
+Settings::Settings(int argc, char ** argv)
+{
+	QCoreApplication::setApplicationName("pat");
+	QCoreApplication::setApplicationVersion("0.0.0");
+
+	QSettings settings("pat.config", QSettings::IniFormat);
+	settings.setIniCodec("UTF-8");
+
+	#ifdef WIN32
+		//path_to_testsystem = "C:/work/pat/examples/build/bin/testsystem.exe";
+		path_to_testsystem = settings.value("/win/path_to_testsystem").toString();
+	#else
+		//path_to_testsystem = "/work/projects/pat/pat/examples/build-stereo/bin/testsystem";
+		path_to_testsystem = settings.value("/lin/path_to_testsystem").toString();
+	#endif
+
+	geometry = settings.value("window/geometry").toRect();
+}
+
+void Settings::SaveSettings(QWidget * win)
+{
+	QSettings settings("pat.config", QSettings::IniFormat);
+	settings.setIniCodec("UTF-8");
+	settings.setValue("window/geometry", win->geometry());
+}
+
+QString Settings::get_path_to_testsystem()
+{
+	return path_to_testsystem;
+}
+
+QRect Settings::get_geometry_window()
+{
+	return geometry;
+}
--- common/settings.hpp
+++ common/settings.hpp
@@ -3,14 +3,22 @@
 #define __SETTINGS_HPP__
 
 #include <QtCore>
+#include <QSettings>
+#include "QWidget"
 
 class Settings
 {
 	public:
 		Settings(int argc, char ** argv);
 
-	private:
+		QString get_path_to_testsystem();
+		QRect   get_geometry_window();
 
+		void SaveSettings(QWidget * win);
+
+	private:
+		QString path_to_testsystem;
+		QRect geometry;
 };
 
 #endif
