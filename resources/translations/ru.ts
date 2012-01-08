e8856175e1bedaff7d5bd4f9ce10d11edc87efd9
AoD314 <morozov.andrey.vmk@gmail.com>
2012-01-09 01:01:13 +0400

    updated Russian translation

--- resources/translations/ru.ts
+++ resources/translations/ru.ts
@@ -1,71 +1,94 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
-    <name>pat::MainWindow</name>
+    <name>SettingsWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="16"/>
-        <source>Application</source>
-        <translation>Приложение</translation>
+        <location filename="settingswindow.cpp" line="12"/>
+        <source>Language</source>
+        <translation>Язык</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="17"/>
-        <source>Exit</source>
-        <translation>Выход</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="19"/>
-        <source>Optimization</source>
-        <translation>Оптимизация</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="20"/>
-        <source>New</source>
-        <translation>Новая</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="21"/>
-        <source>Open</source>
-        <translation>Открыть</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="22"/>
+        <location filename="settingswindow.cpp" line="20"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="24"/>
-        <source>Run/Stop</source>
-        <translation>Запустить/Остановить</translation>
+        <location filename="settingswindow.cpp" line="21"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="33"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>pat::MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="25"/>
+        <source>Application</source>
+        <translation>Приложение</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="26"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Optimization</source>
+        <translation>Оптимизация</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="29"/>
+        <source>New</source>
+        <translation>Новая</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="33"/>
+        <source>Run/Stop</source>
+        <translation>Запустить/Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
         <source>Tools</source>
         <translation>Инструменты</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="27"/>
+        <location filename="mainwindow.cpp" line="36"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="29"/>
+        <location filename="mainwindow.cpp" line="38"/>
         <source>Help</source>
-        <translation>Помощь</translation>
+        <translation>Справка</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="30"/>
-        <location filename="mainwindow.cpp" line="62"/>
+        <location filename="mainwindow.cpp" line="39"/>
+        <location filename="mainwindow.cpp" line="71"/>
         <source>About Application</source>
-        <translation>О приложение</translation>
+        <translation>О программе</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="32"/>
+        <location filename="mainwindow.cpp" line="41"/>
         <source>About Qt</source>
-        <translation>О Qt</translation>
+        <translation>О библиотеке Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="62"/>
+        <location filename="mainwindow.cpp" line="71"/>
         <source> PAT System v0.0.1 </source>
         <translation> PAT System v0.0.1 </translation>
     </message>
