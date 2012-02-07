fa27db3f6db6a5247287bd6bf9909ddb0a2f87b7
AoD314 <morozov.andrey.vmk@gmail.com>
2012-02-08 01:22:52 +0400

    added first version build system on qmake base

---
+++ patconfig.hpp
@@ -0,0 +1,21 @@
+
+#ifndef __PATCONFIG_HPP__
+#define __PATCONFIG_HPP__
+
+#if(defined(WIN32) || defined(_WIN32) || defined(WINCE)) && (defined(PATAPI_EXPORTS))
+    #define PAT_EXPORTS __declspec(dllexport)
+#endif
+
+#if(defined(WIN32) || defined(_WIN32) || defined(WINCE)) && (defined(PATAPI_IMPORTS))
+    #define PAT_EXPORTS __declspec(dllimport)
+#endif
+
+#ifndef PAT_EXPORTS
+    #define PAT_EXPORTS
+#endif
+
+/* Version number of package */
+#define PAT_VERSION "0.0.1"
+
+#endif
+
