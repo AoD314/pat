7189fd0b50291c42eff5e77f2400dd4f9fdce593
AoD314 <morozov.andrey.vmk@gmail.com>
2012-02-26 20:07:59 +0400

    fixed build with Number class

--- pat_gui.cmake
+++
@@ -1,9 +0,0 @@
-#add_executable( ${target_gui} ${gui_srcs} ${gui_hdrs} ${gui_mocs_obj} ${qrc_sources} ${translations})
-add_library(patgui ${gui_srcs} ${gui_hdrs} ${gui_mocs_obj} ${qrc_sources} ${translations})
-target_link_libraries(patgui ${QT_LIBRARIES} ${target_lib})
-
-#if(WIN32)
-	#remove_definitions(-DPATAPI_EXPORTS)
-	#add_definitions   (-DPATAPI_IMPORTS)	
-	set_target_properties(patgui PROPERTIES DEFINE_SYMBOL PATAPI_MPORTS1)
-#endif()
--- pat_lib.cmake
+++
@@ -1,8 +0,0 @@
-add_library( ${target_lib} SHARED ${lib_srcs} ${lib_hdrs} ${lib_mocs_obj})
-target_link_libraries( ${target_lib} ${QT_LIBRARIES})
-
-if(WIN32)
-	#add_definitions   (-DPATAPI_EXPORTS)
-	#remove_definitions(-DPATAPI_IMPORTS)
-	#set_target_properties(${target_lib} PROPERTIES DEFINE_SYMBOL PATAPI_EXPORTS)
-endif()
