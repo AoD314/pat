b9c923becabb73e56be46e7423ae0497ac2cc71d
AoD314 <morozov.andrey.vmk@gmail.com>
2012-10-28 22:59:30 +0400

    renamed req.cmake

--- pat/cmake/lib_version.cmake
+++ pat/cmake/lib_version.cmake
@@ -1,14 +1,4 @@
 
-
-#set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "Configs" FORCE)
-#if(DEFINED CMAKE_BUILD_TYPE)
-#    set_property( CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES} )
-#endif()
-
-#set(CMAKE_BUILD_TYPE "Release")
-
-set(CMAKE_BUILD_TYPE "Debug")
-
 # ----------------------------------------------------------------------------
 #  Get actual pat version number from sources
 # ----------------------------------------------------------------------------
@@ -19,4 +9,3 @@ string(REGEX REPLACE ".+PAT_VERSION_MINOR[ ]+([0-9]+).*" "\\1"  PAT_VERSION_MINO
 string(REGEX REPLACE ".+PAT_VERSION_PATCH[ ]+([0-9]+).*" "\\1"  PAT_VERSION_PATCH  "${pat_version_parts}")
 set(pat_version "${PAT_VERSION_MAJOR}.${PAT_VERSION_MINOR}.${PAT_VERSION_PATCH}")
 # ----------------------------------------------------------------------------
-
